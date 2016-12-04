15443ec5d96268ebb79e4bb3508a552df7afa0fc
Ayyavazhi <kikopeyanski@gmail.com>
2016-12-04 21:44:12 +0200

    Auction-details/comments styles
    -looks cool, trust me i'm a front-ender

--- public/js/offers.js
+++ public/js/offers.js
@@ -1,5 +1,11 @@
-(function () {
-    $('#offer-button').on('click', (e) => {
-        $('#offer-container').toggle();
-    });
-} ());
+$('#offer-button').on('click', function () {
+    if ($('.min-price-and-time').css('display') !== 'none') {
+
+        $('.min-price-and-time').css('display', 'none');
+        $('#offer-container').css('display', 'block');
+
+    }else {
+        $('.min-price-and-time').css('display', 'block');
+        $('#offer-container').css('display', 'none');
+    }
+});
