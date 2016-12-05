f9525d31b3e39eb2abe977029bf1db045e1ac3a3
Merhatt <merhat1998@gmail.com>
2016-12-05 03:47:05 +0200

    Fixed socket bug and route changed to 80

--- utils/constants.js
+++ utils/constants.js
@@ -1,7 +1,7 @@
 /* globals module require*/
 
 const secretWord = "secret";
-const port = 3001;
+const port = 80;
 const usersConnectionString = "mongodb://localhost:27017/Users";
 const auctionsConnectionString = "mongodb://localhost:27017/Users";
 const loggedIn = {
